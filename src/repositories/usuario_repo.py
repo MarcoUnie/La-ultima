784a89aea2798b21eabfc32b884c2b91a6bef15e
MarcoUnie <mmagecoyt@gmail.com>
2025-05-16 09:29:42 +0200

    repositorio usuario

---
+++ src/repositories/usuario_repo.py
@@ -0,0 +1,31 @@
+import json
+import os
+import uuid
+from typing import Optional
+from models.usuario import Usuario
+from repositories.encuesta_repo import DATA_DIR
+USER_FILE = os.path.join(DATA_DIR, "usuarios.json")
+
+class UsuarioRepository:
+    def __init__(self):
+        os.makedirs(DATA_DIR, exist_ok=True)
+        if not os.path.exists(USER_FILE):
+            with open(USER_FILE, "w") as f:
+                json.dump([], f)
+
+    def guardar_usuario(self, usuario: Usuario) -> None:
+        with open(USER_FILE, "r") as f:
+            usuarios = json.load(f)
+        usuarios.append(usuario.__dict__)
+        with open(USER_FILE, "w") as f:
+            json.dump(usuarios, f, default=str, indent=4)
+
+    def obtener_usuario(self, username: str) -> Optional[Usuario]:
+        with open(USER_FILE, "r") as f:
+            usuarios = json.load(f)
+        for data in usuarios:
+            if data.get("username") == username:
+                user = Usuario(username=data["username"], password_hash=data["password_hash"])
+                user.id = uuid.UUID(data["id"])
+                return user
+        return None
